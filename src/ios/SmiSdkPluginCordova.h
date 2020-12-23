06e71bc52ee6c9ae65ae0d2461f004da54e57b8f
Anil Kumar <anil.kumar@datami.com>
2020-12-23 17:43:11 +0530

    Change Note : First version of ionic cordova plugin for test.

---
+++ src/ios/SmiSdkPluginCordova.h
@@ -0,0 +1,28 @@
+//
+//  DatamiSDStateChangePlugin.h
+//  GAIntegration
+//
+//  Created by Damandeep Singh on 09/10/17.
+//
+
+#import <Cordova/CDV.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface SmiSdkPluginCordova : CDVPlugin {
+    
+    NSString* sdStatus;
+    NSString* prevSdStatus;
+    NSString* _callbackId;
+
+}
+- (void)getSDState:(CDVInvokedUrlCommand*)command;
+- (void)getAnalytics:(CDVInvokedUrlCommand*)command;
+- (void)updateUserId:(CDVInvokedUrlCommand*)command;
+- (void)updateUserTag:(CDVInvokedUrlCommand*)command;
+- (void)getSDAuth:(CDVInvokedUrlCommand*)command;
+- (void)startSponsoredData:(CDVInvokedUrlCommand*)command;
+- (void)stopSponsoredData:(CDVInvokedUrlCommand*)command;
+- (void)startSponsorVpn:(CDVInvokedUrlCommand*)command;
+- (void)stopSponsorVpn:(CDVInvokedUrlCommand*)command;
+
+@end
