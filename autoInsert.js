67691eb934d0577c40c8ad0bf7d27c9d8f722281
Pawan Bawankar <pawan.iitroorkee@gmail.com>
2020-12-24 18:54:55 +0530

    location relocated to root of src.

--- autoInsert.js
+++ autoInsert.js
@@ -2,7 +2,7 @@ module.exports = function(ctx) {
     var fs = require('fs'),
         path = require('path');
 
-    var tsScriptPath = path.join(ctx.opts.projectRoot, 'plugins', 'cordova-ionic-smisdk-plugin', 'src', 'android', 'smi-sdk-cordova', 'index.js');
+    var tsScriptPath = path.join(ctx.opts.projectRoot, 'plugins', 'cordova-ionic-smisdk-plugin', 'src', 'smi-sdk-cordova', 'index.js');
     var destLocationPath = path.join(ctx.opts.projectRoot, 'node_modules', '@ionic-native', 'smi-sdk-cordova');
 
     if ( !fs.existsSync( destLocationPath ) ) {
@@ -16,7 +16,7 @@ module.exports = function(ctx) {
         console.log('index.js copied to destination');
     });
 
-    tsScriptPath = path.join(ctx.opts.projectRoot, 'plugins', 'cordova-ionic-smisdk-plugin', 'src', 'android', 'smi-sdk-cordova', 'index.d.ts');
+    tsScriptPath = path.join(ctx.opts.projectRoot, 'plugins', 'cordova-ionic-smisdk-plugin', 'src', 'smi-sdk-cordova', 'index.d.ts');
     destLocationPath = path.join(ctx.opts.projectRoot, 'node_modules', '@ionic-native', 'smi-sdk-cordova', 'index.d.ts');
     fs.copyFile(tsScriptPath, destLocationPath, (err) => {
         if (err) throw err;
